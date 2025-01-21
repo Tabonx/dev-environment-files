510b194e9ef4924064f1a640e962b3bc8c8a66d5
Pavel Kroupa <kroupa.pavel@icloud.com>
2025-01-21 21:38:29 +0100

    Add game layers

--- QMK/Corne-v3/keymap.c
+++ QMK/Corne-v3/keymap.c
@@ -25,8 +25,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define OSM_LSFT OSM(MOD_LSFT)
 #define OSM_HYPR OSM(MOD_HYPR)
 
+
+enum layers{
+  _DEFAULT = 0,
+  _GAME,
+  _GAMESYMBOL,
+  _SYMBOL,
+ _CZSYM,
+  _FUNCTION,
+};
+
+ 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x6_3(
+    [_DEFAULT] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    CZ_Q,    CZ_W,    CZ_E,    CZ_R,    CZ_T,                         CZ_Z,    CZ_U,    CZ_I,    CZ_O,   CZ_P,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -34,12 +45,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      OSM_LSFT,    CZ_Y,    CZ_X,    CZ_C,    CZ_V,    CZ_B,                         CZ_N,    CZ_M, CZ_COMM,  CZ_DOT, CZ_BSLS,  KC_RALT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(1),  KC_ENT,      HYPR_T(KC_SPC), KC_BSPC, MO(2)
+                                          KC_LGUI,   MO(_SYMBOL),  KC_ENT,      HYPR_T(KC_SPC), KC_BSPC, MO(_CZSYM)
                                       //`--------------------------'  `--------------------------'
 
   ),
 
-    [1] = LAYOUT_split_3x6_3(
+    [_SYMBOL] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_RALT, CZ_EXLM,    CZ_AT,CZ_HASH,  CZ_DLR, CZ_TILD,                      CZ_CIRC, CZ_AMPR, CZ_ASTR, CZ_LCBR, CZ_RCBR,  CZ_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -47,11 +58,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      OSM_LSFT,    CZ_6,    CZ_7,    CZ_8,    CZ_9,    CZ_0,                      CZ_QUOT, CZ_PLUS, CZ_SLSH, CZ_LPRN, CZ_RPRN, KC_RALT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_ENT,     KC_SPC,  KC_BSPC , MO(3)
+                                          KC_LGUI, _______,  KC_ENT,     KC_SPC,  KC_BSPC , MO(_FUNCTION)
                                       //`--------------------------'  `--------------------------'
   ),
 
-    [2] = LAYOUT_split_3x6_3(
+    [_CZSYM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB, XXXXXXX, CZ_ECAR, CZ_SCAR, CZ_CCAR, CZ_RCAR,                      CZ_ZCAR, CZ_YACU, CZ_AACU, CZ_IACU, CZ_EACU, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -59,19 +70,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      OSM_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(3),  KC_ENT,     KC_SPC, KC_BSPC, _______
+                                          KC_LGUI,   MO(_FUNCTION),  KC_ENT,     KC_SPC, KC_BSPC, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
-    [3] = LAYOUT_split_3x6_3(
+    [_FUNCTION] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      KC_MPLY, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                    DF(_GAME), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  KC_ENT,     KC_SPC,OSM_HYPR, _______
                                       //`--------------------------'  `--------------------------'
-  )
+  ),
+    [_GAME] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_ESC,    CZ_Q,    CZ_W,    CZ_E,    CZ_R,    CZ_T,                         CZ_Z,    CZ_U,    CZ_I,    CZ_O,   CZ_P,  KC_ESC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       KC_LCTL,    CZ_A,    CZ_S,    CZ_D,    CZ_F,    CZ_G,                         CZ_H,    CZ_J,    CZ_K,    CZ_L,  KC_EQL, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT,    CZ_Y,    CZ_X,    CZ_C,    CZ_V,    CZ_B,                         CZ_N,    CZ_M, CZ_COMM,  CZ_DOT, CZ_BSLS,  KC_RALT,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                         KC_SPC,   MO(_GAMESYMBOL),  KC_LGUI,      HYPR_T(KC_SPC), KC_BSPC, DF(_DEFAULT)
+                                      //`--------------------------'  `--------------------------'
+  ),
+    [_GAMESYMBOL] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      KC_RALT, CZ_EXLM,    CZ_AT,CZ_HASH,  CZ_DLR, CZ_TILD,                      CZ_CIRC, CZ_AMPR, CZ_ASTR, CZ_LCBR, CZ_RCBR,  CZ_GRV,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_TAB,   CZ_PLUS,    CZ_ECAR,    CZ_SCAR,   CZ_CCAR,     CZ_RCAR,                      CZ_MINS,  CZ_EQL, CZ_DQUO, CZ_LBRC, CZ_RBRC, CZ_SCLN,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+     OSM_LSFT,   CZ_ZCAR,    CZ_YACU,    CZ_AACU,    CZ_IACU,    CZ_EACU,                      CZ_QUOT, CZ_PLUS, CZ_SLSH, CZ_LPRN, CZ_RPRN, KC_RALT,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LGUI, _______,  KC_ENT,     KC_SPC,  KC_BSPC , MO(_FUNCTION)
+                                      //`--------------------------'  `--------------------------'
+  ),
+  
 };
