338d1842eb717ab1c0e872a0cb41bdcd3c824bef
Pavel Kroupa <63880977+Tabonx@users.noreply.github.com>
2025-04-04 21:34:34 +0200

    Add Mod-Taps to other layers

--- QMK/Corne-v3/keymap.c
+++ QMK/Corne-v3/keymap.c
@@ -35,6 +35,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define HM_L RALT_T(CZ_L)   // Hold for Option, tap for 'l'
 #define HM_SCLN RGUI_T(KC_EQL) // Hold for Command, tap for ';'
 
+#define HM_1 LGUI_T(CZ_1)   // Hold for Command, tap for '1'
+#define HM_2 LALT_T(CZ_2)   // Hold for Option, tap for '2'
+#define HM_3 LCTL_T(CZ_3)   // Hold for Control, tap for '3'
+#define HM_4 LSFT_T(CZ_4)   // Hold for Shift, tap for '4'
+
+#define HM_EQL RSFT_T(CZ_EQL)   // Hold for Shift, tap for '='
+#define HM_DQUO RCTL_T(CZ_DQUO)   // Hold for Control, tap for '"'
+#define HM_LBRC RALT_T(CZ_LBRC)   // Hold for Option, tap for '['
+#define HM_RBRC RGUI_T(CZ_RBRC) // Hold for Command, tap for ']'
+
 enum layers{
   _DEFAULT = 0,
   _GAME,
@@ -62,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_RALT, CZ_EXLM,    CZ_AT,CZ_HASH,  CZ_DLR, CZ_TILD,                      CZ_CIRC, CZ_AMPR, CZ_ASTR, CZ_LCBR, CZ_RCBR,  CZ_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    CZ_1,    CZ_2,    CZ_3,   CZ_4,     CZ_5,                      CZ_MINS,  CZ_EQL, CZ_DQUO, CZ_LBRC, CZ_RBRC, CZ_SCLN,
+      KC_LCTL,    HM_1,    HM_2,    HM_3,   HM_4,     CZ_5,                      CZ_MINS,  HM_EQL, HM_DQUO, HM_LBRC, HM_RBRC, CZ_SCLN,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      OSM_LSFT,    CZ_6,    CZ_7,    CZ_8,    CZ_9,    CZ_0,                      CZ_QUOT, CZ_PLUS, CZ_SLSH, CZ_LPRN, CZ_RPRN, KC_RALT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -74,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB, XXXXXXX, CZ_ECAR, CZ_SCAR, CZ_CCAR, CZ_RCAR,                      CZ_ZCAR, CZ_YACU, CZ_AACU, CZ_IACU, CZ_EACU, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
+      KC_LCTL, KC_LGUI, KC_RALT, KC_LCTL, KC_LSFT, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      OSM_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -115,5 +125,94 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                           KC_LGUI, _______,  KC_ENT,     KC_SPC,  KC_BSPC , MO(_FUNCTION)
                                       //`--------------------------'  `--------------------------'
   ),
-  
+
 };
+
+// Needed because Mod-Tap doesn't work with shifted keys
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case HM_1:
+            if (!record->tap.count && record->event.pressed) {
+                // Hold: Send LGUI (Command)
+                tap_code16(KC_LGUI);
+                return false;
+            } else if (record->tap.count && record->event.pressed) {
+                // Tap: Send Shift + CZ_1 (Shifted '1' instead of '+')
+                tap_code16(CZ_1); 
+                return false;
+            }
+            return true;
+
+        case HM_2:
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(KC_LALT);
+                return false;
+            } else if (record->tap.count && record->event.pressed) {
+                tap_code16(CZ_2);
+                return false;
+            }
+            return true;
+
+        case HM_3:
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(KC_LCTL);
+                return false;
+            } else if (record->tap.count && record->event.pressed) {
+                tap_code16(CZ_3);
+                return false;
+            }
+            return true;
+
+        case HM_4:
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(KC_LSFT);
+                return false;
+            } else if (record->tap.count && record->event.pressed) {
+                tap_code16(CZ_4);
+                return false;
+            }
+            return true;
+
+        case HM_EQL:
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(KC_RSFT);
+                return false;
+            } else if (record->tap.count && record->event.pressed) {
+                tap_code16(CZ_EQL);
+                return false;
+            }
+            return true;
+
+        case HM_DQUO:
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(KC_RCTL);
+                return false;
+            } else if (record->tap.count && record->event.pressed) {
+                tap_code16(CZ_DQUO);
+                return false;
+            }
+            return true;
+
+        case HM_LBRC:
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(KC_RALT);
+                return false;
+            } else if (record->tap.count && record->event.pressed) {
+                tap_code16(CZ_LBRC);
+                return false;
+            }
+            return true;
+
+        case HM_RBRC:
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(KC_RGUI);
+                return false;
+            } else if (record->tap.count && record->event.pressed) {
+                tap_code16(CZ_RBRC);
+                return false;
+            }
+            return true;
+    }
+
+    return true;
+}
